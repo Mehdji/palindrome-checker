82c63153c3edeacde007932e3b78516729628472
Mehdi <naouimehdi@hotmail.fr>
2025-01-21 21:20:40 +0100

    refactor: remove old README and add sanitization function for palindrome check

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+//Mehdi NAOUI, 2025
+//Palindrome-checker
+//https://github.com/Mehdji/
+//https://x.com/Mehdji87
+//script.js
 const text_input = document.getElementById("text-input");
 const button_input = document.getElementById("check-btn");
 const result_frame = document.getElementById("result");
 const result_text = document.getElementById("result_text");
 
-const text_is_palindrome = `<i class="fa-solid fa-check big-icon"></i>${text_input.value} is a palindrome.`;
-const text_is_not_palindrome = `<i class="fa-solid fa-xmark big-icon"></i>${text_input.value} is not palindrome.`;
+const text_is_palindrome = `<i class="fa-solid fa-check big-icon"></i>${text_input.value} is a palindrome`;
+const text_is_not_palindrome = `<i class="fa-solid fa-xmark big-icon"></i>${text_input.value} is not palindrome`;
 
 //Test if user typed something into input field.
 const is_empty = ()=>{
@@ -12,6 +17,19 @@ const is_empty = ()=>{
         alert("Please input a value");
         return true;
     }
+}
+
+const sanitize = (text)=>{
+    //Convert user's input in lowercase.
+    //Any upper case would trick the test.
+    text = text.toLowerCase()
+    //Compose a regular expression to match some type of symbols and spaces.
+    //Same,some symbols would produce a wrong result.
+    const regex_1 = /[\_\s\,\.\-\\\/\(\)]/g ;
+    //Finally we apply replace method.
+    //All matching characters will be removed.
+    let text_sanitized = text.replace(regex_1,"");
+    return text_sanitized
 }
 
 //Test if user input is a palindrome.
@@ -26,17 +44,18 @@ const is_palindrome = ()=>{
     //.revere() reverse an array;
     //.joint() return a string from an array.
     //when Chained these three methods return a reverted string.    
-    }else if(text_input.value === text_input.value.split("").reverse().join("")){
+    }else if(sanitize(text_input.value) === sanitize(text_input.value.split("").reverse().join(""))){
         result_text.innerHTML = `<i class="fa-solid fa-check big-icon"></i>${text_input.value} is a palindrome.`;
         
     
     }else{
-        result_text.innerHTML = `<i class="fa-solid fa-xmark big-icon"></i>${text_input.value} is not palindrome.`;
+        result_text.innerHTML = `<i class="fa-solid fa-xmark big-icon"></i>${text_input.value} is not a palindrome.`;
     }
     
 }
 
 const all_test = () =>{
+    console.log(sanitize(text_input.value));
     if (is_empty()){
 
     }else{
